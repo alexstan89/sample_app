aa1812174d64d588f4ea1d36234a2baee8431102
Stan Alexandru <stan.alexandru.dan@gmail.com>
2012-06-04 03:43:08 +0200

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,19 @@ before_filter :admin_user,     only: :destroy
   def index
 		@users = User.paginate(page: params[:page])
   end
-  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
   def create
     @user = User.new(params[:user])
     if @user.save
